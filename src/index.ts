65ce8c54714e9fc2014b44c996083dc571944e8c
Clément Murzeau <clement@studiorelief.fr>
2024-02-27 17:13:04 +0100

    Build branch-1.0.0
    First build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
+import './index.css';
+
 import { greetUser } from '$utils/greet';
+import { parallaxAnim, roadPath, stepAnim, teamAnim } from '$utils/gsap';
+import { blogCategories, copyBlog, hideEmpty, spanCMS, swipeElement } from '$utils/jquery';
+import { loadScript } from '$utils/loadScript';
+import { swiperLogoLoop, swiperReferal } from '$utils/swiper';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
+  const name = 'Clément 13.52';
   greetUser(name);
+
+  /*
+  ! Call functions
+  */
+  Promise.all([
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-accordion@1/accordion.js'),
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js'),
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-toc@1/toc.js'),
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-socialshare@1/socialshare.js'),
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-richtext@1/richtext.js'),
+  ]);
+
+  spanCMS();
+  swiperReferal();
+  swiperLogoLoop();
+  blogCategories();
+  swipeElement();
+  hideEmpty();
+  copyBlog();
+
+  parallaxAnim();
+
+  if (window.location.pathname === '/') {
+    roadPath();
+  }
+
+  if (window.location.pathname.includes('/offres')) {
+    stepAnim();
+  }
+
+  if (window.location.pathname.includes('/a-propos')) {
+    teamAnim();
+  }
 });
